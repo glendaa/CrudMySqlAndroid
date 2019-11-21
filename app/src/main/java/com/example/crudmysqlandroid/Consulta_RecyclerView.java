66d1769d88641e87e4b23dc0409bcadaa7dced09
Glendaa <glenda.chanta567@gmail.com>
2019-11-21 10:56:06 -0600

    Se reemplazan layouts y se agrega menus

---
+++ app/src/main/java/com/example/crudmysqlandroid/Consulta_RecyclerView.java
@@ -0,0 +1,14 @@
+package com.example.crudmysqlandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Consulta_RecyclerView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_consulta__recycler_view);
+    }
+}
