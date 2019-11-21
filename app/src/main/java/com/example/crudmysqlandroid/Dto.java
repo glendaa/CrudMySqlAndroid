88681e5d65ef5dfc7e44cf6d36292a01349faa3e
Glendaa <glenda.chanta567@gmail.com>
2019-11-21 10:53:06 -0600

    se reemplaza clase Dto y Config

--- app/src/main/java/com/example/crudmysqlandroid/Dto.java
+++ app/src/main/java/com/example/crudmysqlandroid/Dto.java
@@ -1,20 +1,23 @@
 package com.example.crudmysqlandroid;
 
+
 import java.io.Serializable;
 
+//Para enviar objetos entre actividades (Como parámetro) se coloca el "implements Serializable"
 public class Dto implements Serializable {
     int codigo;
     String descripcion;
     double precio;
 
+
+    public Dto() {
+    }
+
+
     public Dto(int codigo, String descripcion, double precio) {
         this.codigo = codigo;
         this.descripcion = descripcion;
         this.precio = precio;
-    }
-
-    public Dto() {
-
     }
 
     public int getCodigo() {
